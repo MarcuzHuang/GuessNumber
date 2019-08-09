4851a9ea2d83e3b6ee650c6b1c8a09a433229d09
Marcuz.Huang <marcuz.huang@gmail.com>
2019-08-09 17:57:39 +0800

    fullversion

--- GuessNumber.py
+++ GuessNumber.py
@@ -2,3 +2,18 @@
 #讓使用者重複輸入數字去猜
 #猜對的話 印出"終於猜對了!"
 #猜錯的話 印出"比答案大或小"
+
+import random
+r = random.randint(1,100)
+
+while True:
+	ans = input('請輸入一數字介於1~100 : ')
+	ans = int(ans)
+	if ans == r :
+		print('終於猜對了!')
+		break
+	else:
+		if ans < r :
+			print('您的數字較答案來的小')
+		else:
+			print('您的數字較答案來的大')
